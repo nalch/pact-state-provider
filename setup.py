0349724de1ac18ed8852413e44803fafac61f57a
nalch <scholze.kristian@gmail.com>
2019-12-29 10:59:17 +0000

    Prepare version 1.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = ['setuptools', 'click']
 
 setup(
     name='pact_state_provider',
-    version='1.0.4',
+    version='1.0.6',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
