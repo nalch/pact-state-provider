d3bb246601f2fb32d4a39ce2c715219a7f7429c4
Kristian Scholze <kristian.scholze@consultant.renre.com>
2020-01-03 09:12:44 +0000

    Prepare version 1.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ INSTALL_REQUIRES = ['setuptools', 'click']
 
 setup(
     name='pact_state_provider',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
